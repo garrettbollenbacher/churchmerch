24ee74880cd9ce52e862ec1a02b6e9c39fd84b8b
garrettbollenbacher <garrett.bollenbacher@gmail.com>
2024-11-26 13:34:49 -0500

    made title home link

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100">
+        <div className="w-full p-4 fixed top-0 left-0 bg-white shadow-md flex justify-between items-center">
+          <Link
+            to="/"
+            className="text-2xl font-bold text-gray-900 ml-4 flex items-center title-link"
+          >
+            <span>church</span>
+            {/* <img
+              src="/churchmerch-cross.png"
+              alt="Cross"
+              className="mx-2 h-6 w-6"
+            /> */}
+            <span>merch</span>
+          </Link>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-20">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -39,16 +39,11 @@ const HomePage: React.FC = () => {
 
   return (
     <>
-      <div className="w-full p-4 flex justify-start items-start">
-        <Link to="/" className="text-lg font-bold text-blue-600">
-          Home
-        </Link>
-      </div>
       <div className="flex flex-col items-center justify-center min-h-screen w-full">
         <div className="container max-w-4xl mx-auto px-4 py-8 text-center">
-          <h1 className="text-5xl font-bold mb-8 text-gray-900">
+          {/* <h1 className="text-5xl font-bold mb-8 text-gray-900">
             Apparel For The Body of Christ
-          </h1>
+          </h1> */}
           <input
             type="text"
             value={searchTerm}
